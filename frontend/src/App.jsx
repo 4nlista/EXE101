fca02cc3a9741a25b264eed6efce03940aea46e4
Nguyễn Bảo An <khiconst4@gmail.com>
2026-07-20 17:04:51 +0700

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>EXE101 Frontend</h1>
+      <p>React + Vite Setup Successful!</p>
+    </div>
+  );
+}
+
+export default App;
