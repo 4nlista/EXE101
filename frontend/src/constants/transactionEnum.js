03693ae873a0ba767133348e6ebfb4ecce14a259
Nguyễn Bảo An <khiconst4@gmail.com>
2026-09-24 14:15:29 +0700

    chỉnh enum

--- frontend/src/constants/transactionEnum.js
+++ frontend/src/constants/transactionEnum.js
@@ -2,10 +2,11 @@
 export const TRANSACTION_STATUS = {
   PENDING: 'pending',   // Đang chờ xử lý thanh toán
   SUCCESS: 'success',   // Thanh toán thành công
-  FAILED: 'failed'      // Thanh toán thất bại
+  FAILED: 'failed',     // Thanh toán thất bại
+  REFUNDED: 'refunded'  // Đã hoàn tiền
 };
 
 // Phương thức thanh toán
 export const PAYMENT_METHOD = {
-  VNPAY: 'vnpay'    // Thanh toán qua VNPay
+  SEPAY: 'sepay'    // Thanh toán qua VietQR SePay
 };
